066c9f6386a5b34ff6252af4fa6b7d571832ae7c
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2021-03-10 22:35:56 -0300

    feat: add winston on logger lib

--- libs/logger/src/index.ts
+++ libs/logger/src/index.ts
@@ -1,1 +1,9 @@
-export * from './lib/logger';
+import * as winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
