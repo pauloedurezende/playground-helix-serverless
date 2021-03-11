38843bc7c31aa06d3278df37e912de997973ee33
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2021-03-10 22:46:16 -0300

    feat: support string interpolation on logger

--- libs/logger/src/index.ts
+++ libs/logger/src/index.ts
@@ -1,9 +1,9 @@
-import * as winston from 'winston';
+import { transports, format, createLogger } from 'winston';
 
-const logger = winston.createLogger({
+const logger = createLogger({
   level: 'info',
-  format: winston.format.simple(),
-  transports: [new winston.transports.Console()],
+  format: format.combine(format.splat(), format.simple()),
+  transports: [new transports.Console()],
 });
 
 export default logger;
