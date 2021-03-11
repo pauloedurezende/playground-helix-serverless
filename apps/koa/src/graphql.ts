887dc6388b3ce8385371cbb71e8f6066ccfdd040
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2021-03-10 22:46:55 -0300

    feat: add basic setup for koa server

---
+++ apps/koa/src/graphql.ts
@@ -0,0 +1,7 @@
+import KoaRouter from 'koa-router';
+
+const router = KoaRouter();
+
+router.all('/graphql', () => {});
+
+export default router;
--- apps/koa/src/main.ts
+++ apps/koa/src/main.ts
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+import logger from '@playground/logger';
+import app from './app';
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  logger.log('info', 'GraphQL server is running on port %s', port);
+});
